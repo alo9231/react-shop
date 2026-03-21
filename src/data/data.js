cc5637ebf685eb430f15c2ddae0cb7786f75570a
jiseon.lee <aloc0107@naver.com>
2026-03-21 18:33:41 +0900

    axios 추가

--- src/data/data.js
+++ src/data/data.js
@@ -1,27 +1,30 @@
-import img1 from "../img/shoes1.jpg";
-import img2 from "../img/shoes2.jpg";
-import img3 from "../img/shoes3.jpg";
-
-export const myImages = [
-  { 
-    id: 0, 
-    title: "White and Black", 
-    content: "Born in France", 
-    price: 120000,
-    src: img1 
-  },
-  { 
-    id: 1, 
-    title: "Red Knit", 
-    content: "Born in Seoul", 
-    price: 110000,
-    src: img2 
-  },
-  { 
-    id: 2, 
-    title: "Grey Yordan", 
-    content: "Born in the States", 
-    price: 130000,
-    src: img3
-   }
-  ];
+export const myProducts = [
+  {
+    "isLast": false,
+    "totalPage": 3,
+    "data" : [
+          { 
+        id: 0, 
+        title: "White and Black", 
+        content: "Born in France", 
+        price: 120000,
+        src: "/img/shoes1.jpg"
+      },
+      { 
+        id: 1, 
+        title: "Red Knit", 
+        content: "Born in Seoul", 
+        price: 110000,
+        src: "/img/shoes2.jpg"
+      },
+      { 
+        id: 2, 
+        title: "Grey Yordan", 
+        content: "Born in the States", 
+        price: 130000,
+        src: "/img/shoes3.jpg"
+      },
+    ]
+  }
+]
+;
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,12 +1,13 @@
 import { useParams } from 'react-router-dom';
 
-function Detail({myImages}){
+function Detail({myProducts}){
 
     // 주소창의 :id 값을 가져옴
     let { id } = useParams();
 
     // 배열에서 해당 순서의 데이터 꺼내기
-    let selectProduct = myImages[id];
+    // props로 받은 myProducts(실제로는 shoes)에서 id가 일치하는 상품 찾기
+    let selectProduct = myProducts.find((x )=>( x.id == id))
 
     // 만약 데이터가 없을 때를 대비한 안전장치 (매우 중요!)
     if (!selectProduct) {
@@ -16,7 +17,7 @@ function Detail({myImages}){
   return(
      <div className="row">
         <div className="col-md-6">
-          <img src={selectProduct.src} width="100%" alt={selectProduct.title}/>
+          <img src={import.meta.env.BASE_URL + selectProduct.src} width="100%" alt={selectProduct.title}/>
         </div>
         <div className="col-md-6">
           <strong className="pt-5">{selectProduct.title}</strong>
